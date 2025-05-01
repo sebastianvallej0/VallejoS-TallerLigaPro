e613837ec9f34575907244ca6a3547509a769f62
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-05-01 00:10:03 -0500

    Creo el modelo jugador para seguir con lo demas

---
+++ VallejoS-TallerLigaPro/Models/Jugador.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using VallejoS_TallerLigaPro.Models;
+
+namespace VallejoS_LigaPro.Models
+{
+    public class Jugador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Nombre del jugador")]
+        public string Nombre { get; set; }
+
+        [Range(1, 99)]
+        [DisplayName("Número de camiseta")]
+        public int NumeroCamiseta { get; set; }
+
+        [Range(0, int.MaxValue)]
+        [DisplayName("Goles")]
+        public int Goles { get; set; }
+
+        [Range(0, int.MaxValue)]
+        [DisplayName("Asistencias")]
+        public int Asistencias { get; set; }
+
+        [Range(0, double.MaxValue)]
+        [DisplayName("Sueldo")]
+        public double Sueldo { get; set; }
+
+        public int EquipoId { get; set; }
+        public Equipo Equipo { get; set; }
+    }
+}
