f8167369fe6388e7270f87f0c3bf32fe7248e9e1
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-04-30 22:04:46 -0500

    Agregar archivos de proyecto.

---
+++ VallejoS-TallerLigaPro/Models/Equipo.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace VallejoS_TallerLigaPro.Models
+{
+    
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del equipo es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+        [DisplayName("Nombre del equipo")]
+        public string Nombre { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Los partidos jugados deben estar entre 0 y 100.")]
+        [DisplayName("Partidos jugados")]
+        public int PartidosJugados { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Los partidos ganados deben estar entre 0 y 100.")]
+        [DisplayName("Partidos ganados")]
+        public int PartidosGanados { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Los partidos empatados deben estar entre 0 y 100.")]
+        [DisplayName("Partidos empatados")]
+        public int PartidosEmpatados { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Los partidos perdidos deben estar entre 0 y 100.")]
+        [DisplayName("Partidos perdidos")]
+        public int PartidosPerdidos { get; set; }
+
+        
+        [DisplayName("Puntos totales")]
+        public int Puntos => PartidosGanados * 3 + PartidosEmpatados;
+
+        
+        public bool ValidarPartidos() => PartidosJugados == (PartidosGanados + PartidosEmpatados + PartidosPerdidos);
+    }
+}
---
+++ VallejoS-TallerLigaPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VallejoS_TallerLigaPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
